1bc1bbb8cfbceabcf22e09a9391a4206018c3538
Unknown <471811570@qq.com>
2018-11-15 18:59:47 +0800

    代码补完

--- main.c
+++ main.c
@@ -10,37 +10,39 @@
 //接收路由信息的线程
 void *thr_fn(void *arg)
 {
-	int st=0;
+	char *ifname;
 	struct selfroute *selfrt; 
 	selfrt = (struct selfroute*)malloc(sizeof(struct selfroute));
-	memset(selfrt,0,sizeof(struct selfroute));
+	memset(selfrt, 0, sizeof(struct selfroute));
 
-	//get if.name
-	struct if_nameindex *head, *ifni;
-	ifni = if_nameindex();
-  	head = ifni;
-	char *ifname;
+	int sock_fd;
+	struct sockaddr_in server_addr;
+	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-	// add-24 del-25
+	bzero(&server_addr, sizeof(struct) sockaddr_in);
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server_addr.sin_port = htons(800);
+
+	bind(sock_fd, (struct sockaddr *)(&server_addr), sizeof(struct sockaddr));
+	listen(sock_fd, 5);
+	
 	while(1)
 	{
-		st=static_route_get(selfrt);
-		if(st == 1)
+		int conn_fd = accept(sock_fd, (struct sockaddr *)NULL, NULL);
+		ret = recv(conn_fd, selfrt, sizeof(selfroute), 0);
+		if(selfrt->cmdnum == 24)
 		{
-			if(selfrt->cmdnum == 24)
-			{
-				if_indextoname(selfrt->ifindex, ifname);
-				insert_route(selfrt->prefix, selfrt->prefixlen, 
-					selfrt->ifname, selfrt->ifindex, selfrt->nexthop);
-				
-			}
-			else if(selfrt->cmdnum == 25)
-			{
-				delete_route(selfrt->prefix, selfrt->prefixlen);
-				//从路由表里删除路由
-			}
+			if_indextoname(selfrt->ifindex, ifname);
+			insert_route(selfrt->prefix, selfrt->prefixlen, 
+				selfrt->ifname, selfrt->ifindex, selfrt->nexthop);
+			
 		}
-
+		else if(selfrt->cmdnum == 25)
+		{
+			delete_route(selfrt->prefix, selfrt->prefixlen);
+		}
+		close(conn_fd);
 	}
 
 }
--- recvroute.c
+++ recvroute.c
@@ -2,19 +2,5 @@
 
 int static_route_get(struct selfroute *selfrt)
 {
-	int sock_fd;
-	struct sockaddr_in server_addr;
-	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-	bzero(&server_addr, sizeof(struct) sockaddr_in);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-	server_addr.sin_port = htons(800);
-
-	bind(sock_fd, (struct sockaddr *)(&server_addr), sizeof(struct sockaddr));
-	listen(sock_fd, 5);
-
-	int conn_fd = accept(sock_fd, (struct sockaddr *)NULL, NULL);
-	ret = recv(conn_fd, selfrt, sizeof(selfroute), 0);
-	close(conn_fd);
+	
 }
